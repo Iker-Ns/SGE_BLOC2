50f1d5fccca25b92c2eaefa30b0243f95ac5a2b5
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-01-28 09:05:14 +0100

    Update

--- postgresql_python/main.py
+++ postgresql_python/main.py
@@ -1,7 +1,3 @@
-from read_registre import read_reg
+from update_registre import update_reg
 
-results = read_reg()
-
-for result in results:
-    print(f"Nom: {result[0]}, Adreça: {result[1]}, Telèfon: {result[2]}, Email: '{result[3]}', Neixament: {result[4]}")
-    print("\n")
+print(update_reg())
---
+++ postgresql_python/update_registre.py
@@ -0,0 +1,18 @@
+import connect
+
+def update_reg():
+    conn = connect.connection_db()
+    cursor = conn.cursor()
+
+    sql_update = """
+        UPDATE clientes
+        SET teléfono_cliente = '000000000'
+        WHERE nombre_cliente = 'Roger'
+    """
+    cursor.execute(sql_update)
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+    return {"Update Successfully"}
